8dfdf67a79b5fd863a801f7c47449efd51fb8ff9
Abdelali El makkioui <abel-mak@e2r6p9.1337.ma>
2021-10-13 15:17:12 +0100

    add to tree iterator

--- src/tree_iterator.hpp
+++ src/tree_iterator.hpp
@@ -6,20 +6,39 @@
 /*   By: abel-mak <abel-mak@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 16:46:03 by abel-mak          #+#    #+#             */
-/*   Updated: 2021/10/12 18:36:17 by abel-mak         ###   ########.fr       */
+/*   Updated: 2021/10/13 15:13:30 by abel-mak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TREE_ITERATOR_HPP
 #define TREE_ITERATOR_HPP
 
+#include <iterator>
+
 namespace ft
 {
-	template <typename type>
-	class tree_iterator
+	template <typename Y>
+	struct tree_node
 	{
-	public:
-		typedef type value_type;
+		typedef tree_node *node_ptr;
+		typedef const tree_node *const_node_ptr;
+
+		node_ptr parent;
+		node_ptr left;
+		node_ptr right;
+	};
+
+	template <typename Y>
+	struct tree_iterator
+	{
+		typedef Y value_type;
+		typedef Y &reference;
+		typedef Y *pointer;
+		typedef std::bidirectional_iterator_tag iterator_category;
+		typedef ptrdiff_t difference_type;
+		typedef tree_node<Y> *node_ptr;
+
+		node_ptr node;
 	};
 }  // namespace ft
 
