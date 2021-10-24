6e0d76ad46e1ee3212fd8ea0f041a2a32ca90100
Abdelali El makkioui <abel-mak@e2r5p8.1337.ma>
2021-10-24 19:05:35 +0100

    add small update

--- src/tree.hpp
+++ src/tree.hpp
@@ -6,12 +6,14 @@
 /*   By: abel-mak <abel-mak@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 16:48:31 by abel-mak          #+#    #+#             */
-/*   Updated: 2021/10/23 19:14:17 by abel-mak         ###   ########.fr       */
+/*   Updated: 2021/10/24 19:04:37 by abel-mak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TREE_HPP
 #define TREE_HPP
+
+#include "tree_iterator.hpp"
 
 namespace ft
 {
@@ -26,6 +28,11 @@ namespace ft
 		typedef typename allocator_type::const_pointer const_pointer;
 		typedef typename allocator_type::difference_type difference_type;
 		typedef typename allocator_type::size_type size_type;
+		typedef tree_node<Y> *node_ptr;
+
+	private:
+		node_ptr _end_node;
+		node_ptr _start_node;
 	};
 }  // namespace ft
 
