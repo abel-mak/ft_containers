1b6084dd66cb30faff73be81352e843dce26c225
Abdelali El makkioui <abel-mak@e2r4p8.1337.ma>
2021-10-27 19:22:51 +0100

    add rotation related stuff

--- src/tree.hpp
+++ src/tree.hpp
@@ -6,15 +6,18 @@
 /*   By: abel-mak <abel-mak@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 16:48:31 by abel-mak          #+#    #+#             */
-/*   Updated: 2021/10/26 19:17:25 by abel-mak         ###   ########.fr       */
+/*   Updated: 2021/10/27 19:22:22 by abel-mak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TREE_HPP
 #define TREE_HPP
 
+#include <string>
+
 #include "tree_iterator.hpp"
 #include "utility.hpp"
+
 /*
  * left-left imbalanced do right rotation
  * right-right imbalced do left rotation
@@ -48,6 +51,10 @@ namespace ft
 		pair<iterator, bool> insert(const value_type &v);
 		node_ptr find(K &k);
 		node_ptr constructNode(const value_type &x);
+		void balanceAfterInsert(node_ptr x);
+		std::string getRotation(node_ptr x);
+		void leftRotate(node_ptr x);
+		void rightRotate(node_ptr x);
 	};
 	template <typename K, typename V, typename Comp, typename Alloc>
 	tree<K, V, Comp, Alloc>::tree(void)
@@ -67,34 +74,81 @@ namespace ft
 		return (newN);
 	}
 	template <typename K, typename V, typename Comp, typename Alloc>
+	std::string tree<K, V, Comp, Alloc>::getRotation(node_ptr x)
+	{
+		int bf;
+		std::string res = "";
+		int i;
+
+		i = 0;
+		while (i < 2 && ((bf = height(x->right) - height(x->left) < -1) ||
+		                 (bf = height(x->right) - height(x->left) > 1)))
+		{
+			if (bf > 1 || bf < -1)
+			{
+				if (bf < 0)
+				{
+					x = x->left;
+					res += "l";
+					// left heavy
+				}
+				else
+				{
+					x = x->right;
+					res += "r";
+					// right heavy
+				}
+			}
+			i++;
+		}
+	}
+	template <typename K, typename V, typename Comp, typename Alloc>
+	void tree<K, V, Comp, Alloc>::balanceAfterInsert(node_ptr x)
+	{
+		node_ptr child;
+		node_ptr grandChild;
+
+		while (x != &_rootParentNode)
+		{
+			x = x->parent;
+		}
+	}
+	template <typename K, typename V, typename Comp, typename Alloc>
 	pair<typename tree<K, V, Comp, Alloc>::iterator, bool>
 	tree<K, V, Comp, Alloc>::insert(const value_type &x)
 	{
 		K key;
 		node_ptr child;
 		node_ptr newN;
+		pair<iterator, bool> res;
 
 		key   = x.first;
 		child = this->find(key);
-		if (key < child->value.first || child == &_rootParentNode)
+		if (key != child->value.first)
 		{
 			newN         = constructNode(x);
-			child->left  = newN;
 			newN->parent = child;
-			// insert to left of the child
-		}
-		else if (key > child->value.first)
-		{
-			newN         = constructNode(x);
-			child->right = newN;
-			newN->parent = child;
-			// insert to right of the child
+			res.second   = true;
+			if (key < child->value.first || child == &_rootParentNode)
+			{
+				child->left = newN;
+				// insert to left of the child
+			}
+			else if (key > child->value.first)
+			{
+				child->right = newN;
+				// insert to right of the child
+			}
 		}
 		else
 		{
-			newN = child;
+			newN       = child;
+			res.second = false;
 			// the keys are equal return child
 		}
+		res.first = iterator(child);
+		if (res.second == true)
+			this->balance_after_insert(child);
 	}
 	template <typename K, typename V, typename Comp, typename Alloc>
 	typename tree<K, V, Comp, Alloc>::node_ptr tree<K, V, Comp, Alloc>::find(
